40041ba47e14a4cd1fcefdc1de090f06f993cc5f
Freeman-md <freemanmadudili@gmail.com>
2021-09-24 21:43:50 +0100

    Setup Base Template Layout

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint # splits up application into multiple files that perform specific actions
+from flask import Blueprint, render_template
+# Blueprint splits up application into multiple files that perform specific actions
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # Decorator
 def home(): 
-  return '<h1>Test</h1>'
+  return render_template('home.html')
