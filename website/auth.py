7e55786cce1c457bce8c7ca6d09c52db0bebe6ee
Freeman-md <freemanmadudili@gmail.com>
2021-09-25 05:15:14 +0100

    Implemented Registration

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -27,5 +30,11 @@ def sign_up():
     elif len(password1) < 7:
       flash('Password must be at least characters', category='error')
     else:
+      new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
+      db.session.add(new_user)
+      db.session.commit()
+
       flash('Account created!', category='success')
+      return redirect(url_for('views.home'))
+
   return render_template('sign_up.html')
