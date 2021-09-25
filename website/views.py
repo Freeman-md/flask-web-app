7758a3f52d855be42634a80a6ec4c659c0c19890
Freeman-md <freemanmadudili@gmail.com>
2021-09-25 05:37:23 +0100

    Loading User Data and Authorization

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
 # Blueprint splits up application into multiple files that perform specific actions
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # Decorator
+@login_required
 def home(): 
-  return render_template('home.html')
+  return render_template('home.html', user=current_user)
